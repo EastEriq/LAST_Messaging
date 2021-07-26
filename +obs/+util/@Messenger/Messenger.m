827716bfb5188bf89f13fcd53dae22c075f8e29d
Enrico Segre <enrico.segre@weizmann.ac.il>
2021-07-26 16:35:39 +0300

    typo in comment

--- +obs/+util/@Messenger/Messenger.m
+++ +obs/+util/@Messenger/Messenger.m
@@ -3,7 +3,7 @@ classdef Messenger < obs.LAST_Handle % not of class handle, if has to have a pri
     properties
         DestinationHost='localhost'; % Destination host. Named or IP. localhost valid
         DestinationPort=50001; % Port on the destination host
-        LocalPort=50000; % port on the local host. Can be same as DestinationPort if Destination hot is not localhost
+        LocalPort=50000; % port on the local host. Can be same as DestinationPort if DestinationHost is not localhost
         EnablePortSharing='off'; % if 'on', different processses on the same localhost can receive on the same port
         Name % free text, useful for labelling the Messenger object
         MessagesSent=0; % incrementing number of messages sent
