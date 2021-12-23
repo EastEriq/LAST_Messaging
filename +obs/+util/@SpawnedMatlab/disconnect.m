4f09b4610d79638679524bf3bdc8ae7682caca63
Enrico Segre (last04) <enrico.segre@weizmann.ac.il>
2021-12-23 08:16:49 +0000

    better treatment of SpawnedMatlab.disconnect cases

--- +obs/+util/@SpawnedMatlab/disconnect.m
+++ +obs/+util/@SpawnedMatlab/disconnect.m
@@ -1,17 +1,27 @@
 function disconnect(S)
     % try to quit gracefully the spawned session (if it responds normally)
+
     if isa(S.Messenger,'obs.util.Messenger')
+
         % we check S.Status, because the logic is already in there.
         % If it is 'disconnected', we do not waste time in
-        %  checking communinication and getting a timeout without need.
-        alive=strcmp(S.Status,'alive');
-        if alive
+        %  checking communication and getting a timeout without need.
+        status=S.Status;
+        if strcmp(status,'alive')
             S.Messenger.send('exit')
         end
-        % if the slave is dead/unresponsive, or send times out, kill
-        if ~alive || ~isempty(S.Messenger.LastError)
+
+        % if the slave is dead/unresponsive, or send() times out, kill it
+        if isempty(S.Messenger.LastError)
+            S.PID=[];
+            S.Status='disconnected';
+        else
             S.report(['graceful exit of slave session ' S.Id ...
-                ' timed out, attempting to kill\n'])
+                ' timed out'])
+        end
+
+        if ~isempty(S.PID)
+            S.report('attempting to kill slave session %s\n',S.Id)
             S.kill
         end
     end
