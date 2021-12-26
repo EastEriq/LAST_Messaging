58f626b105a8d043aef12726dbe37cd427caea83
Enrico Segre (last04) <enrico.segre@weizmann.ac.il>
2021-12-26 10:33:38 +0000

    delete but not report invalid Messenger StreamResources on disconnect

--- +obs/+util/@Messenger/disconnect.m
+++ +obs/+util/@Messenger/disconnect.m
@@ -1,11 +1,14 @@
 function disconnect(Msng)
-% close the serial stream, but don't delete it from workspace
+% close the udp stream, but don't delete it from workspace
 %  and delete the receiver callback function (policy for both still TBD)
     if isvalid(Msng.StreamResource)
         Msng.StreamResource.DatagramReceivedFcn='';
         fclose(Msng.StreamResource);
     else
-        Msng.reportError(['invalid udp resource while attempting to ',...
-                          'disconnect messenger %s'], Msng.Id)
+        % delete it only if invalid (can happen when recreating
+        %  an object, when the old is deleted)
+        delete(Msng.StreamResource)
+%         Msng.reportError(['invalid udp resource while attempting to ',...
+%                           'disconnect messenger %s'], Msng.Id)
     end
 end
