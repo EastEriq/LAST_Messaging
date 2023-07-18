2b44e9d1d4726485968a83fef42f54e1a6b3b2db
Enrico Segre <enrico.segre@weizmann.ac.il>
2023-07-18 21:34:00 +0300

    correction Messenger.getMyAttention

--- +obs/+util/@Messenger/getMyAttention.m
+++ +obs/+util/@Messenger/getMyAttention.m
@@ -1,13 +1,13 @@
 function resp=getMyAttention(Msng)
 % tell to the messenger receiving on the other end, to change its
 %  DestinationHost and DestinationPort so to answer here.
-% This is useful for taking over, when w know that there is a messenger
+% This is useful for taking over, when we know that there is a messenger
 %  receiving at the other end, but it was bound to another messenger
 %  elsewhere
 % We evaluate in the remote datagramParser, but the settings of the
 %  StreamResource are globally visible
      % java trick to get the hostname, from matlabcentral
     localhostname=char(java.net.InetAddress.getLocalHost.getHostName);
-    send(Msng,['Msng.DestinationHost=''' localhostname ''';'],true);
-    send(Msng,['Msng.DestinationPort=' num2str(Msng.LocalPort) ';'],true);
+    send(Msng,['Msng.DestinationHost=''' localhostname ''';'],false,true);
+    send(Msng,['Msng.DestinationPort=' num2str(Msng.LocalPort) ';'],false,true);
     resp=Msng.areYouThere;
