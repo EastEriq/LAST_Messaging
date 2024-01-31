cabc8ca3c1f59f7a746d9b2412e89260da175b76
The LAST Ocs <>
2024-01-31 17:34:37 +0000

    if .listeners is not able to rsh, report it and don't try Responder ports

--- +obs/+util/@SpawnedMatlab/listeners.m
+++ +obs/+util/@SpawnedMatlab/listeners.m
@@ -10,14 +10,14 @@ function [Mpids,Rpids]=listeners(S)
     end
     
     if isa(S.Messenger,'obs.util.Messenger')
-        [~,a]=system(sprintf('ssh %s %s%s "lsof -ti :%d"', ...
+        [s,a]=system(sprintf('ssh %s %s%s "lsof -ti :%d"', ...
             S.SshOptions, user, S.Messenger.DestinationHost, S.Messenger.DestinationPort));
     else
         if isempty(S.Messenger)
             % if the Messenger has not yet been created, check using the
             %  values of the relevant properties known to the SpawnedMatlab
             %  object itself
-            [~,a]=system(sprintf('ssh %s %s%s "lsof -ti :%d"', ...
+            [s,a]=system(sprintf('ssh %s %s%s "lsof -ti :%d"', ...
                 S.SshOptions, user, S.Host, S.MessengerRemotePort));
         else
             S.reportError('.Messenger is not a messenger')
@@ -25,10 +25,13 @@ function [Mpids,Rpids]=listeners(S)
         end
     end
     Mpids=str2num(a);
+    if s~=0 && s~=1 % return status can be 1 if lsof didn't find the port in use
+        S.reportError('cannot execute commands on %s',S.Host)
+    end
     
-    if nargout>1
+    if nargout>1 && (s==0 || s==1)
         if isa(S.Responder,'obs.util.Messenger')
-            [~,a]=system(sprintf('ssh -o %s %s%s "lsof -ti :%d"', ...
+            [~,a]=system(sprintf('ssh %s %s%s "lsof -ti :%d"', ...
                 S.SshOptions, user, S.Responder.DestinationHost, S.Responder.DestinationPort));
         else
             if isempty(S.Responder)
@@ -40,4 +43,6 @@ function [Mpids,Rpids]=listeners(S)
             end
         end
         Rpids=str2num(a);
+    else
+        Rpids=[];
     end
