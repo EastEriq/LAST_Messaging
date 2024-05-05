50bc0eeea0ba2079497a3817e7e03eef28c0a6d2
The LAST Ocs <>
2024-05-05 10:05:12 +0000

    improve bash messenger query example

--- doc/MessengerQuery.sh
+++ doc/MessengerQuery.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+[ "$1" = "-h" -o "$1" = "--help" -o $# -ne 3 ] && echo "
 # proof of concept communication wth a matlab Messenger using shell commands
 
 # Instructions: in matlab: create a Messenger object
@@ -9,23 +9,23 @@
 
 # In the shell: MessengerQuery.sh <host> <port> <command>
 #
-# Examples: ./MessengerQuery.sh localhost 40000 "atand(1)"
-#           ./MessengerQuery.sh localhost 40000 "a=3.4"
-#           ./MessengerQuery.sh localhost 40000 "a/3"
-
+# Examples: ./MessengerQuery.sh localhost 40000 \"atand\(1\)\"
+#           ./MessengerQuery.sh localhost 40000 \"a=3.4\"
+#           ./MessengerQuery.sh localhost 40000 \"a/3\"
+" && exit
 
 HOST=$1
 PORT=$2
 COMMAND=$3
-TIMESTAMP=739107.64072227245 # read a compatible timestamp, but not really important
-LOCALPORT=55555 # or better, find the port used by nc
+# date %s gives seconds from 1/1/1970, Matlab timestamp is days since 0/1/0000
+TIMESTAMP=`bc <<<  "scale=9; $(( $(date +%s) ))/86400.0+719529"`
 
-JSTRING=`echo '{"ReplyTo":{"Host":"localhost","Port":'$LOCALPORT'},'\
-              '"SentTimestamp":'\$TIMESTAMP',"ReceivedTimestamp":[],'\
-              '"ProgressiveNumber":2,"Command":"'$COMMAND'",'\
+JSTRING=`echo '{"ReplyTo":{"Host":"localhost","Port":[]},'\
+              '"SentTimestamp":'$TIMESTAMP',"ReceivedTimestamp":[],'\
+              '"ProgressiveNumber":'$RANDOM',"Command":"'$COMMAND'",'\
               '"RequestReplyWithin":10,"Content":[],"EvalInListener":false}'`
 
 #echo $JSTRING
 
-echo $JSTRING | nc -uC -W 1 -q 1 -w 2 -p $LOCALPORT $HOST $PORT
+echo -n $JSTRING | nc -uC -W 1 -q 1 -w 2 $HOST $PORT | jq
 echo
